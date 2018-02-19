9a53efd5f5b2ac841f34d037ff44b3d31793e2b5
Michael Fiano <mail@michaelfiano.com>
2018-02-19 16:36:01 -0500

    Updated to reflect newer gamebox-math etc api's

--- example/src/components/stub-0/package.lisp
+++ example/src/components/stub-0/package.lisp
@@ -1,6 +1,7 @@
 (in-package :defpackage+-1)
 
 (defpackage+ #:first-light-example.comp.stub-0
+  (:use #:cl)
   (:inherit #:fl.core)
   (:export-only #:stub-0
                 #:value))
--- src/components/stub-0/stub-0.lisp
+++
@@ -1,8 +0,0 @@
-(in-package :fl.comp.stub-0)
-
-(define-component stub-0 ()
-  (value 42))
-
-(defmethod initialize-component ((component stub-0) (context context))
-  (format t "fl.comp.stub-0:initialize-component called: ~A~%"
-          (value component)))
